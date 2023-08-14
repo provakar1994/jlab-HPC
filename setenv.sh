e24164c627bcaff019f7e0a528e76070c663353c
Sebastian Seeds <sseeds@ifarm1901.jlab.org>
2023-08-14 18:21:55 -0400

    configured setenv.sh script for local environment

--- setenv.sh
+++ setenv.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# sseeds - Updated 8.9.23 to personal ifarm environment
+
 # -------------------------------------------------------------------------- #
 # This script sets various environmet paths reuired by the "submit-" scripts #
 # Environment paths are user specific, hence all the entries in this script  #
@@ -12,21 +14,21 @@
 # -------------------------------------------------------------------------- #
 
 # Required by all
-export SCRIPT_DIR=/Path/to/jlab-HPC/repository
+export SCRIPT_DIR=/w/halla-scshelf2102/sbs/seeds/jlab-HPC
 
 # Required by the scripts running G4SBS simulations
-export G4SBS=/Path/to/G4SBS/install/directory
+export G4SBS=/w/halla-scshelf2102/sbs/seeds/sim/install
 
 # Required by the scripts running SIMC (simc_gfortran) jobs
-export SIMC=/Path/to/simc_gfortran/repository
+export SIMC=/w/halla-scshelf2102/sbs/seeds/simc_gfortran
 
 # Required by the scripts running digitization jobs using sbsdig
-export LIBSBSDIG=/Path/to/libsbsdig/install/directory
+export LIBSBSDIG=/w/halla-scshelf2102/sbs/seeds/dig/install
 
 # Required by the scripts running replay (data or MC) jobs
-export ANALYZER=/Path/to/analyzer/install/directory
-export SBSOFFLINE=/Path/to/SBS-offline/install/directory
-export SBS_REPLAY=/Path/to/SBS-replay/repository
+export ANALYZER=/w/halla-scshelf2102/sbs/seeds/ANALYZER/install
+export SBSOFFLINE=/w/halla-scshelf2102/sbs/seeds/SBS_OFFLINE/install
+export SBS_REPLAY=/w/halla-scshelf2102/sbs/seeds/SBS_Replay_upstream/SBS-replay
 
 #Path to data directories
 #The path is written this way below becauses strings will need to be added to the left side of them, ie /cache/$GMN_DATA_PATH
